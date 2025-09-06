19b927f4dc903d5c3a704c6e69dbdd6dd1e55369
MohammedHussien10 <86535395+MohammedHussien10@users.noreply.github.com>
2025-09-06 21:20:15 +0300

    Initial Commit

---
+++ Movie-Listing-App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Movie-Listing-App
+//
+//  Created by Macos on 06/09/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Movie-Listing-AppUITests/Movie_Listing_AppUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Movie_Listing_AppUITestsLaunchTests.swift
+//  Movie-Listing-AppUITests
+//
+//  Created by Macos on 06/09/2025.
+//
+
+import XCTest
+
+final class Movie_Listing_AppUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
